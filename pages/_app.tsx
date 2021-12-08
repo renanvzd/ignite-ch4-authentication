fd171bc2482979a6cc1bde5cc1a064a1a0d800c8
Renan Veronez Drechsler <renanvz@outlook.com>
2021-12-08 19:36:58 -0300

    AuthContext

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
 import { AppProps } from 'next/app'
+import { AuthProvider } from '../contexts/AuthContext'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  )
 }
 
 export default MyApp
