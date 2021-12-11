4043d07d8890283a644607c94cf916b67f1ad924
Renan Veronez Drechsler <renanvz@outlook.com>
2021-12-11 19:09:45 -0300

    Validando permissoes (Server)

--- frontend/hooks/useCan.ts
+++ frontend/hooks/useCan.ts
@@ -1,5 +1,6 @@
 import { useContext } from "react";
 import { AuthContext } from "../contexts/AuthContext";
+import { validateUserPermissions } from "../utils/validateUserPermissions";
 
 type UseCanParams = {
   permissions?: string[];
@@ -13,25 +14,10 @@ export function useCan({ permissions, roles }: UseCanParams) {
     return false;
   }
 
-  if (permissions?.length > 0) {
-    const hasAllPermissions = permissions.every(permission => {
-      return user.permissions.includes(permission);
-    });
-
-    if (!hasAllPermissions) {
-      return false;
-    }
-  }
-
-  if (roles?.length > 0) {
-    const hasAllRoles = roles.some(role => {
-      return user.roles.includes(role);
-    });
-
-    if (!hasAllRoles) {
-      return false;
-    }
-  }
-
-  return true;
+  const userHasValidPermissions = validateUserPermissions({
+    user,
+    permissions,
+    roles
+  })
+  return userHasValidPermissions;
 }
